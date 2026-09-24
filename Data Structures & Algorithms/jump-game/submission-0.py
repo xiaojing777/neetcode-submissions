5dab52b6c70e0d3727d25824ed321fa2c2ebdf49
Xiaojing Sun <xiaojingsun8@gmail.com>
2026-09-24 10:01:20 +0800

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        cover = 0
+        for i in range(len(nums)):
+            if i>cover:
+                return False
+            cover = max(cover, i+nums[i])
+
+        
+        return True
+        
+
+        
